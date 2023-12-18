2628ba8efd2aaa4103ee9cd857f1833f7c8300f8
laurafullstack <laurapajaresmartin@hotmail.com>
2023-12-18 12:39:40 +0100

    guion antes de nombre

--- persistencia.py
+++ persistencia.py
@@ -2,5 +2,5 @@
 def guardar_pedido(nombre, apellidos):
     """Module persistencia función"""
     with open("pedidos.txt", "a",encoding="UTF-8")as file:
-        file.write(nombre + " " + apellidos + "\n")
+        file.write("-" + nombre + " " + apellidos + "\n")
         file.close()
